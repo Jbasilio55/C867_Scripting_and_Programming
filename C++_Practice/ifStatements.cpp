32d3915521c3b9122e5539b265ff020762973e4e
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-02 15:57:07 -0400

    if statement practice

--- C++_Practice/ifStatements.cpp
+++ C++_Practice/ifStatements.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
+    bool isMale = true;
+    bool isTall = true;
+
+    if(isMale && isTall)
+    {
+        cout << "You are a tall male \n";
+    }
+    else
+    {
+        cout << "You are not tall and not a male \n";
+    }
+
+    bool isRich = false;
+
+    if(isMale && isTall && isRich)
+    {
+        cout << "You will get all the ladies \n";
+    }
+    else if(isMale || isTall)
+    {
+        cout << "You get some ladies \n";
+    }else
+    {
+        cout << "You get no ladies \n";
+    }
 
     return 0;
 }
