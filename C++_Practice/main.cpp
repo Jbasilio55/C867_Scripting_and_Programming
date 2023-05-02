fcb86b6c91df5456cafb2d1abd4f7d91286a4f06
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-02 10:11:13 -0400

    C++ varriable and literals

--- C++_Practice/main.cpp
+++ C++_Practice/main.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 int main()
 {
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+
     cout << "   /|" << endl;
     cout << "  / |" << endl;
     cout << " /  |" << endl;
     cout << "/___|" << endl;
-    
+
+    cout << "There was once a mane name " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
+
     return 0;
 }
