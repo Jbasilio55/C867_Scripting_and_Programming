7db258aaaa29dd0ec0cfd7226dec06d78ae1d453
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-02 13:59:14 -0400

    simple calculator

---
+++ small_projects/calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter in first number: ";
+    cin >> num1;
+
+    cout << "Enter in second number: ";
+    cin >> num2;
+
+    cout << "total: " << num1 + num2 << endl;
+
+    return 0;
+}
