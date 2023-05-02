5f67ecc7762d0cb2a8b8b74348e433f96ab95fc3
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-02 15:10:55 -0400

    function practice complete

--- C++_Practice/functions.cpp
+++ C++_Practice/functions.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+// declare a function
+void test();
+
+// pass in argument as parameters
+void sayHi(string name, int age)
+{
+    cout << "Hello " << name << ", you are " << age << " years old" << endl;
+}
+
 int main()
 {
+    //script is run from top to bottom
+    cout << "Top \n";
+    sayHi("Sam", 29);
+    cout << "Bottom \n";
+
+    sayHi("Will", 30);
+    sayHi("Sarah", 17);
+
+    test();
 
     return 0;
+}
+
+void test()
+{
+    cout << "Testing functions \n";
 }
